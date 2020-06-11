85344d53611132391e70005532db4e9cc8428f9d
Samuel Offler <sam.offler@gmail.com>
2020-06-11 13:00:18 +0100

    Fixed Grid

--- grid.py
+++ grid.py
@@ -51,10 +51,6 @@ class Cell():
         # Set the neighbour list.
         self._initialiseNeighbours(w, h)
 
-    def position(self):
-        """ Return the coordinates of the cell as a tuple (x, y). """
-        return (self._x, self._y)
-
     def left(self):
         """ Return the coordinates of the cell to the left as a tuple (x, y). """
         if self._neighbour_list[0] is None:
@@ -130,7 +126,7 @@ class Cell():
             self._neighbours += 1
 
         # Cell isn't on top edge.
-        if self._y < w:
+        if self._y < h-1:
             self._neighbour_list[3] = self._y + 1
             self._neighbours += 1
 
@@ -206,11 +202,18 @@ class Grid():
             y -- The y position of the cell.
         """
 
-        # Fill the cell.
-        self._cells[x][y].fill()
+        # Make sure the cell is empty.
+        if not self._cells[x][y].occupied():
 
-        # Add the cell to the 'filled' list.
-        self._filled.append((x, y))
+            # Fill the cell.
+            self._cells[x][y].fill()
+
+            # Add the cell to the 'filled' list.
+            self._filled.append((x, y))
+
+            # Check the internal state is correct.
+            if debug:
+                self._validate()
 
     def empty(self, x, y, debug=False):
         """ Empty a cell in the the grid.
@@ -227,3 +230,89 @@ class Grid():
 
             # Remove the cell from the filled list.
             self._filled.remove((x, y))
+
+            # Check the internal state is correct.
+            if debug:
+                self._validate()
+
+    def _validate(self):
+        """ Assert that the internal state of the Grid is correct. """
+
+        # Loop over all cell rows.
+        for x in range(0, self._w):
+
+            # Loop over all cell columns.
+            for y in range(0, self._h):
+
+                # Assert that the cell appears in the 'filled' list the correct
+                # number of times.
+                assert self.cell(x, y).occupied() == self._filled.count((x, y))
+
+def test_grid_fill():
+    """ Test the fill method for the grid class. """
+
+    # Intialise a 10x10 grid.
+    g = Grid(10, 10)
+
+    # Tally counter for the number of filled cells.
+    n = 0
+
+    # Let's check that the fill method works for all
+    # cells in the grid. Probably overkill, but what the heck!
+
+    # Loop over the width of the grid.
+    for w in range(0, g.width()):
+
+        # Loop over the height of the grid.
+        for h in range(0, g.height()):
+
+            # Increment the number of cells.
+            n += 1
+
+            # Fill the cell.
+            g.fill(w, h)
+
+            # Check that the number of filled cells is correct.
+            assert g.nFilled() == n
+
+            # Check that this cell is marked as filled.
+            assert g.cell(w, h).occupied()
+
+            # Try filling the cell again. (Not allowed!)
+            g.fill(w, h)
+
+            # Check that the number of filled cells is correct.
+            assert g.nFilled() == n
+
+def test_grid_empty():
+    """ Test the empty method for the grid class. """
+
+    # Intialise a 10x10 grid.
+    g = Grid(10, 10)
+
+    # Let's check that the empty method works for all
+    # cells in the grid. Probably overkill, but what the heck!
+
+    # Loop over the width of the grid.
+    for w in range(0, g.width()):
+
+        # Loop over the height of the grid.
+        for h in range(0, g.height()):
+
+            # Fill the cell.
+            g.fill(w, h)
+
+            # Check that their is one filled cell.
+            assert g.nFilled() == 1
+
+            # Check that this cell is marked as filled.
+            assert g.cell(w, h).occupied()
+
+            # Empty the cell.
+            g.empty(w, h)
+
+            # Check that their are no filled cells.
+            assert g.nFilled() == 0
+
+            # Check that this cell is marked as empty.
+            assert not g.cell(w, h).occupied()
